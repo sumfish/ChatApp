d034eb5db847140e5d6d04ab5718871cd74d1f4d
sumfish <f74036190@mail.ncku.edu.tw>
2021-03-13 23:58:04 +0800

    add start menu

--- app/src/main/java/com/example/chatapp/StartActivity.java
+++ app/src/main/java/com/example/chatapp/StartActivity.java
@@ -1,13 +1,27 @@
 package com.example.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+
+    Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        register=findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
